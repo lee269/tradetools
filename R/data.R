7f4b4fad7892ecaa725569b33a9368285c0d2e37
David Lee <david@lee269.co.uk>
2017-08-27 15:53:50 +0000

    Ongoing dataset documentation

--- R/data.R
+++ R/data.R
@@ -4,31 +4,74 @@
 #' download file (SMKE19).
 #'
 #' @format A data frame with 100 rows and 23 variables:
-#' \describe{
-#'   \item{comcode}{character}
-#'   \item{sitc}{character}
-#'   \item{record_type}{character}
-#'   \item{cod_seq}{character}
-#'   \item{cod_alpha}{character}
-#'   \item{account_mm}{character}
-#'   \item{account_ccyy}{character}
-#'   \item{port_seq}{character}
-#'   \item{port_alpha}{character}
-#'   \item{flag_seq}{character}
-#'   \item{flag_alpha}{character}
-#'   \item{trade_ind}{character}
-#'   \item{container}{character}
-#'   \item{mode_of_transport}{character}
-#'   \item{inland_mot}{character}
-#'   \item{golo_seq}{character}
-#'   \item{golo_alpha}{character}
-#'   \item{suite_indicator}{character}
-#'   \item{procedure_code}{character}
-#'   \item{value}{double}
-#'   \item{quantity_1}{double}
-#'   \item{quantitiy_2}{double}
-#'   \item{industrial_plant_comcode}{character}
+#'
+#' \tabular{lll}{
+#' \strong{Name} \tab \strong{Type} \tab \strong{Description}\cr
+#' comcode                  \tab character \tab 8 digit commodity code\cr
+#' sitc                     \tab character \tab SITC code\cr
+#' record_type              \tab character \tab Range 0-3 identifies the kind of
+#' suppression record\cr
+#'
+#' cod_seq                  \tab character \tab Numeric sequence for Country of
+#' Destination\cr
+#'
+#' cod_alpha                \tab character \tab Alpha code for Counry of
+#' Destination\cr
+#'
+#' account_mm               \tab character \tab Identifies the month of account
+#' of an item in format MM. For non-amendment records this will be the same as
+#' the file month; for amendment records it will be the month to which the
+#' amendment relates.\cr
+#'
+#' account_ccyy             \tab character \tab Identifies the century and year
+#' of an item in format CCYY. For non-amendment records this will be the same as
+#' the file month; for amendment records it will be the year to which the
+#' amendment relates.\cr
+#'
+#' port_seq                 \tab character \tab Numeric code for port of export\cr
+#' port_alpha               \tab character \tab Alpha code for port of export\cr
+#' flag_seq                 \tab character \tab Numeric sequence for the flag
+#' (nationality) of the ship. Zero for Trade Indicator ‘5’ items.\cr
+#'
+#' flag_alpha               \tab character \tab Numeric sequence for the flag
+#' (nationality) of the ship. Zero for Trade Indicator ‘5’ items.\cr
+#'
+#' trade_ind                \tab character \tab A code used to identify the type
+#' of trade.\cr
+#'
+#' container                \tab character \tab Code identifying goods
+#' transported by container: 0 = not in container, 1 = in container\cr
+#'
+#' mode_of_transport        \tab character \tab Mode of transport by which goods
+#' leave or arrive in the UK\cr
+#'
+#' inland_mot               \tab character \tab Optional code identifying the
+#' mode of transport where there has been an ‘internal’ movement within the
+#' Community before goods have been subject to Customs formalities\cr
+#'
+#' golo_seq                 \tab character \tab Numeric sequence for Location
+#' alpha (golo_alpha)\cr
+#'
+#' golo_alpha               \tab character \tab Alpha code indicating location
+#' of inland clearance (inland clearance depot or inland rail depot) if goods
+#' were not cleared at the border. Also used to indicate relevant free zone
+#' location for goods entering or leaving freezone.\cr
+#'
+#' suite_indicator          \tab character \tab Identifies commodity codes to be
+#' excluded from General and Special trade compilations\cr
+#'
+#' procedure_code           \tab character \tab This identifies specific types
+#' of Customs Procedure\cr
+#'
+#' value                    \tab double    \tab Value (£)\cr
+#' quantity_1               \tab double    \tab Quantity\cr
+#' quantitiy_2              \tab double    \tab Quantity (alternate measure)\cr
+#' industrial_plant_comcode \tab character \tab With effect from January 2011, a
+#' pseudo concessionary comcode beginning 98 used for declaration of high value
+#' Industrial Plant.
+#'
 #' }
+#'
 #' @source \url{http://www.uktradeinfo.com/}
 "sample_data_exports"
 
@@ -39,31 +82,75 @@
 #' download file (SMKI19).
 #'
 #' @format A data frame with 100 rows and 23 variables:
-#' \describe{
-#'   \item{comcode}{character}
-#'   \item{sitc}{character}
-#'   \item{record_type}{character}
-#'   \item{cod_seq}{character}
-#'   \item{cod_alpha}{character}
-#'   \item{account_mm}{character}
-#'   \item{account_ccyy}{character}
-#'   \item{port_seq}{character}
-#'   \item{port_alpha}{character}
-#'   \item{flag_seq}{character}
-#'   \item{flag_alpha}{character}
-#'   \item{trade_ind}{character}
-#'   \item{container}{character}
-#'   \item{mode_of_transport}{character}
-#'   \item{inland_mot}{character}
-#'   \item{golo_seq}{character}
-#'   \item{golo_alpha}{character}
-#'   \item{suite_indicator}{character}
-#'   \item{procedure_code}{character}
-#'   \item{value}{double}
-#'   \item{quantity_1}{double}
-#'   \item{quantitiy_2}{double}
-#'   \item{industrial_plant_comcode}{character}
+#'
+#' \tabular{lll}{
+#' \strong{Name} \tab \strong{Type} \tab \strong{Description}\cr
+#' comcode                  \tab character \tab 8 digit commodity code\cr
+#' sitc                     \tab character \tab SITC code\cr
+#'
+#' record_type              \tab character \tab Range 0-3 identifies the kind of
+#' suppression record\cr
+#'
+#' cod_seq                  \tab character \tab Numeric sequence for Country of
+#' Dispatch\cr
+#'
+#' cod_alpha                \tab character \tab Alpha code for Country of
+#' Dispatch\cr
+#'
+#' account_mm               \tab character \tab Identifies the month of account
+#' of an item in format MM. For non-amendment records this will be the same as
+#' the file month; for amendment records it will be the month to which the
+#' amendment relates.\cr
+#'
+#' account_ccyy             \tab character \tab Identifies the century and year
+#' of an item in format CCYY. For non-amendment records this will be the same as
+#' the file month; for amendment records it will be the year to which the
+#' amendment relates.\cr
+#'
+#' port_seq                 \tab character \tab Numeric code for port of import\cr
+#' port_alpha               \tab character \tab Alpha code for port of import\cr
+#' flag_seq                 \tab character \tab Numeric sequence for the flag
+#' (nationality) of the ship. Zero for Trade Indicator ‘5’ items.\cr
+#'
+#' flag_alpha               \tab character \tab Numeric sequence for the flag
+#' (nationality) of the ship. Zero for Trade Indicator ‘5’ items.\cr
+#'
+#' trade_ind                \tab character \tab A code used to identify the type
+#' of trade.\cr
+#'
+#' container                \tab character \tab Code identifying goods
+#' transported by container: 0 = not in container, 1 = in container\cr
+#'
+#' mode_of_transport        \tab character \tab Mode of transport by which goods
+#' leave or arrive in the UK\cr
+#'
+#' inland_mot               \tab character \tab Optional code identifying the
+#' mode of transport where there has been an ‘internal’ movement within the
+#' Community before goods have been subject to Customs formalities\cr
+#'
+#' golo_seq                 \tab character \tab Numeric sequence for Location
+#' alpha (golo_alpha)\cr
+#'
+#' golo_alpha               \tab character \tab Alpha code indicating location
+#' of inland clearance (inland clearance depot or inland rail depot) if goods
+#' were not cleared at the border. Also used to indicate relevant free zone
+#' location for goods entering or leaving freezone.\cr
+#'
+#' suite_indicator          \tab character \tab Identifies commodity codes to be
+#' excluded from General and Special trade compilations\cr
+#'
+#' procedure_code           \tab character \tab This identifies specific types
+#' of Customs Procedure\cr
+#'
+#' value                    \tab double    \tab Value (£)\cr
+#' quantity_1               \tab double    \tab Quantity\cr
+#' quantitiy_2              \tab double    \tab Quantity (alternate measure)\cr
+#' industrial_plant_comcode \tab character \tab With effect from January 2011, a
+#' pseudo concessionary comcode beginning 98 used for declaration of high value
+#' Industrial Plant.
+#'
 #' }
+#'
 #' @source \url{http://www.uktradeinfo.com/}
 "sample_data_imports"
 
@@ -76,23 +163,27 @@
 #' dispatch download file (SMKM46/SMKX46).
 #'
 #' @format A data frame with 100 rows and 17 variables:
-#' \describe{
-#'   \item{comcode}{character}
-#'   \item{record_type}{character}
-#'   \item{cod_seq}{character}
-#'   \item{cod_alpha}{character}
-#'   \item{trade_ind}{character}
-#'   \item{coo_seq}{character}
-#'   \item{nature_of_transaction}{character}
-#'   \item{mode_of_transport}{character}
-#'   \item{period_reference}{character}
-#'   \item{suite_indicator}{character}
-#'   \item{sitc}{character}
-#'   \item{ip_comcode}{character}
-#'   \item{stat_value}{double}
-#'   \item{nett_mass}{double}
-#'   \item{supp_unit}{double}
+#' \tabular{lll}{
+#' \strong{Name} \tab \strong{Type} \tab \strong{Description}\cr
+#' comcode               \tab character \tab x\cr
+#' record_type           \tab character \tab x\cr
+#' cod_seq               \tab character \tab x\cr
+#' cod_alpha             \tab character \tab x\cr
+#' trade_ind             \tab character \tab x\cr
+#' coo_seq               \tab character \tab x\cr
+#' coo_alpha             \tab character \tab x\cr
+#' nature_of_transaction \tab character \tab x\cr
+#' mode_of_transport     \tab character \tab x\cr
+#' period_reference      \tab character \tab x\cr
+#' suite_indicator       \tab character \tab x\cr
+#' sitc                  \tab character \tab x\cr
+#' ip_comcode            \tab character \tab x\cr
+#' no_of_consignments    \tab double    \tab x\cr
+#' stat_value            \tab double    \tab x\cr
+#' nett_mass             \tab double    \tab x\cr
+#' supp_unit             \tab double    \tab x
 #' }
+#'
 #' @source \url{http://www.uktradeinfo.com/}
 "sample_data_arrdis"
 
@@ -105,12 +196,16 @@
 #' dispatch estimate download file (SESM16/SESX16).
 #'
 #' @format A data frame with 100 rows and 4 variables:
-#' \describe{
-#'   \item{sitc_2}{character}
-#'   \item{sitc_0}{character}
-#'   \item{month_ind}{character}
-#'   \item{estimated_value}{double}
+#'
+#' \tabular{lll}{
+#' \strong{Name} \tab \strong{Type} \tab \strong{Description}\cr
+#' sitc_2          \tab character \tab x\cr
+#' sitc_0          \tab character \tab x\cr
+#' month_ind       \tab character \tab x\cr
+#' estimated_value \tab double    \tab x
 #' }
+#'
+#'
 #' @source \url{http://www.uktradeinfo.com/}
 "sample_data_arrdisest"
 
@@ -122,38 +217,77 @@
 #' (SMKA12).
 #'
 #' @format A data frame with 100 rows and 31 variables:
-#' \describe{
-#'   \item{comcode}{character}
-#'   \item{intra_extra_ind}{character}
-#'   \item{intra_mm_on}{character}
-#'   \item{intra_yy_on}{character}
-#'   \item{intra_mm_off}{character}
-#'   \item{intra_yy_off}{character}
-#'   \item{extra_mm_on}{character}
-#'   \item{extra_yy_on}{character}
-#'   \item{extra_mm_off}{character}
-#'   \item{extra_yy_off}{character}
-#'   \item{non_trade_id}{character}
-#'   \item{sitc_no}{character}
-#'   \item{sitc_ind}{character}
-#'   \item{sitc_conv_a}{character}
-#'   \item{sitc_conv_b}{character}
-#'   \item{cn_q2}{character}
-#'   \item{supp_arrivals}{character}
-#'   \item{supp_dispatches}{character}
-#'   \item{supp_imports}{character}
-#'   \item{supp_exports}{character}
-#'   \item{sub_group_arr}{character}
-#'   \item{item_arr}{character}
-#'   \item{sub_group_disp}{character}
-#'   \item{item_disp}{character}
-#'   \item{sub_group_imp}{character}
-#'   \item{item_imp}{character}
-#'   \item{sub_group_exp}{character}
-#'   \item{item_exp}{character}
-#'   \item{qty1_alpha}{character}
-#'   \item{qty2_alpha}{character}
-#'   \item{commodity_alpha}{character}
+#' \tabular{lll}{
+#' \strong{Name} \tab \strong{Type} \tab \strong{Description}\cr
+#' comcode         \tab character \tab x\cr
+#' intra_extra_ind \tab character \tab x\cr
+#' intra_mm_on     \tab character \tab x\cr
+#' intra_yy_on     \tab character \tab x\cr
+#' intra_mm_off    \tab character \tab x\cr
+#' intra_yy_off    \tab character \tab x\cr
+#' extra_mm_on     \tab character \tab x\cr
+#' extra_yy_on     \tab character \tab x\cr
+#' extra_mm_off    \tab character \tab x\cr
+#' extra_yy_off    \tab character \tab x\cr
+#' non_trade_id    \tab character \tab x\cr
+#' sitc_no         \tab character \tab x\cr
+#' sitc_ind        \tab character \tab x\cr
+#' sitc_conv_a     \tab character \tab x\cr
+#' sitc_conv_b     \tab character \tab x\cr
+#' cn_q2           \tab character \tab x\cr
+#' supp_arrivals   \tab character \tab x\cr
+#' supp_dispatches \tab character \tab x\cr
+#' supp_imports    \tab character \tab x\cr
+#' supp_exports    \tab character \tab x\cr
+#' sub_group_arr   \tab character \tab x\cr
+#' item_arr        \tab character \tab x\cr
+#' sub_group_disp  \tab character \tab x\cr
+#' item_disp       \tab character \tab x\cr
+#' sub_group_imp   \tab character \tab x\cr
+#' item_imp        \tab character \tab x\cr
+#' sub_group_exp   \tab character \tab x\cr
+#' item_exp        \tab character \tab x\cr
+#' qty1_alpha      \tab character \tab x\cr
+#' qty2_alpha      \tab character \tab x\cr
+#' commodity_alpha \tab character \tab x
 #' }
+#'
 #' @source \url{http://www.uktradeinfo.com/}
 "sample_data_control"
+
+
+
+
+
+
+#' HMRC trader data
+#'
+#' A dataset containing a random sample of 100 records of HMRC importer/exporter
+#' details data. The data is in the format returned by the
+#' \code{\link{read_hmrc_traders}} function in the trade tools package. The
+#' original files on the \url{www.uktradeinfo.com} site are named
+#' Importers/Exporters.
+#'
+#'
+#' @format A data frame with 100 rows and 31 variables:
+#'
+#'
+#' \tabular{lll}{
+#' \strong{Name}        \tab \strong{Type}      \tab \strong{Description}\cr
+#' year        \tab double    \tab Year\cr
+#' month       \tab double    \tab Month\cr
+#' period      \tab integer   \tab Period in form YYYYMM\cr
+#' nolines     \tab integer   \tab Number of lines for the company\cr
+#' companyname \tab character \tab Name of company\cr
+#' address1    \tab character \tab Address line 1\cr
+#' address2    \tab character \tab Address line 2\cr
+#' address3    \tab character \tab Address line 3\cr
+#' address4    \tab character \tab Address line 4\cr
+#' address5    \tab character \tab Address line 5\cr
+#' postcode    \tab character \tab Postcode\cr
+#' codenum     \tab character \tab Sequence number of commodity code\cr
+#' comcode     \tab character \tab Commodity code
+#' }
+#'
+#' @source \url{http://www.uktradeinfo.com/}
+"sample_data_traders"
